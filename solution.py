915da9da96fbf87298bbab638768a7175c182b7f
MrDipo <42739211+MrDipo@users.noreply.github.com>
2022-11-30 23:59:58 +0100

    save to db not inserting

--- solution.py
+++ solution.py
@@ -1,4 +1,4 @@
-import random, statistics
+import random, statistics, psycopg2
 
 WEEK = {
     "MONDAY": "GREEN, YELLOW, GREEN, BROWN, BLUE, PINK, BLUE, YELLOW, ORANGE, CREAM, ORANGE, RED, WHITE, BLUE, WHITE, BLUE, BLUE, BLUE, GREEN".split(", "),
@@ -46,16 +46,52 @@ def median_color(colors=COLORS):
     return f"Median color is {statistics.median(colors)}"
 
 
-def variance_color(week):
-    pass
+def variance_color(colors=COLORS, color_values=COLOR_VALUES):
+    sum = 0
+    squared_diff_sum = 0
+    for color in colors:
+        sum += color_values[color]
+    mean = sum/len(colors)
+    for color in colors:
+        squared_diff_sum += (color_values[color] - mean)**2
+    variance = squared_diff_sum/len(colors)
+    return variance
 
-def probability_color(week, color):
-    pass
 
-def save_to_db(color_freq):
-    pass
+def probability_color(colors=COLORS, color='RED'):
+    return f"Probability of {color} being chosen is {round(colors.count(color)/len(colors), 3)}"
 
-def binary_search(n, nums):
+
+def save_to_db(colors=COLORS, color_value=COLOR_VALUES):
+    def create_table():
+        conn = psycopg2.connect("dbname='colors' user='postgres' password='0000' host='localhost' port='5432'")
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS colors (color TEXT, frequency INTEGER)")
+        conn.commit()
+        conn.close()
+
+    def insert(color, frequency):
+        conn = psycopg2.connect("dbname='colors' user='postgres' password='0000' host='localhost' port='5432'")
+        cur = conn.cursor()
+        cur.execute("INSERT INTO colors VALUES(%s,%s)", (color,frequency))
+        conn.commit()
+        conn.close()
+
+    def view():
+        conn = psycopg2.connect("dbname='colors' user='postgres' password='0000' host='localhost' port='5432'")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM colors")
+        rows = cur.fetchall()
+        conn.close()
+        return rows
+    
+    create_table()
+    for color in color_value.keys():
+        insert(color, colors.count(color))
+    view()
+
+
+def binary_search(n=69, nums=[1,2,3,4,7,3,2,4,6,9,43,23,554,65,123,78,9,6345,235,465,3123,69]):
     nums.sort()
     def recursive(left, right):
         if left <= right:
@@ -78,8 +114,10 @@ def num_generator():
     base10_num = int(base2_num, 2)
     return f"Binary number generated: {base2_num}, Decimal Equivalent: {base10_num}"
     
-
-def fib(n=50):
-    if n == 0: return 0
-    elif n == 1: return 1
-    else: return fib(n-1)+fib(n-2)
+cache = {0:0, 1:1}
+def fib(n):
+    if n in cache: # base case
+        return cache[n]
+    # Compute and cache the Fibonacci number
+    cache[n] = fib(n - 1) + fib(n - 2)  # Recursive case
+    return cache[n]
